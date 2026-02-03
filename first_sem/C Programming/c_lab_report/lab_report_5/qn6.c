ee81dc3045a372cc487dc307c2b8c446303a50cb
pranish <pranishrestha@outlook.com>
2026-02-03 18:35:25 +0545

    Solving Lab report 5

---
+++ first_sem/C Programming/c_lab_report/lab_report_5/qn6.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main (){
+        int a,b;
+    printf("Enter Order of matrix:");
+    scanf("%d%d",&a,&b);
+    int max[a][b],trans[a][b] ;
+    printf("Enter Elements of Martix 1:");
+    for ( int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            scanf("%d",&max[i][j]);
+        }
+    }
+    for ( int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            trans[i][j]=max[j][i];
+        }
+    }
+        printf("The transpose of Matrices is :\n");
+    for ( int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            printf("%d ",trans[i][j]);
+
+        }
+        printf(" \n");
+    }
+}
