8b0e438059944a85e61ebed4e30e9ae59d9d23ae
Pranish <pranishrestha@outlook.com>
2026-03-17 11:48:50 +0545

    Solved c lab 11 all

--- first_sem/c_programming/c_lab_report/lab_report_11/qn4.c
+++ first_sem/c_programming/c_lab_report/lab_report_11/qn4.c
@@ -4,9 +4,17 @@
 int main()
 {
     int gd = DETECT, gm;
-
     initgraph(&gd, &gm, (char*)"");
-    
+for (int i = 0; i < 600; i+=5)
+{
+    cleardevice();
+    line(0,260,650,260);
+    rectangle(130+i,170,220+i,200);
+    rectangle(100+i,200,250+i,250);
+    circle(130+i,250,10);
+    circle(220+i,250,10);
+    delay(20);
+    }
     getch();
     closegraph();
     return 0;
---
+++ first_sem/c_programming/c_lab_report/lab_report_11/qn5.c
@@ -0,0 +1,27 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char*)"");
+    setlinestyle(SOLID_LINE, 0, 1);
+    line(20, 30, 200, 30);
+    setlinestyle(DOTTED_LINE, 0, 1);
+    line(20, 50, 200, 50);
+    setlinestyle(DASHED_LINE, 0, 1);
+    line(20, 70, 200, 70);
+    setlinestyle(CENTER_LINE, 0, 1);
+    line(20, 90, 200, 90);
+    for (int i = 0; i < 7; i++) {
+        setcolor(i);
+        rectangle(20 + i * 40, 140, 50 + i * 40, 170);
+    }
+    for (int i = 0; i < 6; i++) {
+        setfillstyle(i, i + 1);
+        bar(20 + i * 50, 210, 60 + i * 50, 250);
+    }
+    getch();
+    closegraph();
+    return 0;
+}
