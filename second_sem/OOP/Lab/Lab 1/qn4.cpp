2cf48314d1eda2f210534a73edd59a7266493c93
Pranish <pranishrestha@outlook.com>
2026-07-13 19:54:06 +0545

    Adding Lab works and notes

--- second_sem/OOP/Lab/Lab 1/qn4.cpp
+++ second_sem/OOP/Lab/Lab 1/qn4.cpp
@@ -28,32 +28,15 @@ class swapValue{
     }
 };
 int main(){
-    int choice;
     int a,b;
     swapValue s1;
-    cout<<"1. Swap by value\n2. Swap by pointer\n3. Swap by reference\nEnter your choice: ";
-    cin>>choice;
-    switch (choice)
-    {
-    case 1:
-        cout<<"Enter two numbers: ";
-        cin>>a>>b;
-        s1.swapByValue(a,b);
-        break;
-    case 2:
-        cout<<"Enter two numbers: ";
-        cin>>a>>b;
-        s1.swapByPointer(&a,&b);
-        break;
-    case 3:
-        cout<<"Enter two numbers: ";
-        cin>>a>>b;
-        s1.swapByReference(a,b);
-        break;
-    
-    default:
-        cout<<"Invalid choice!"<<endl;
-        break;
-    }
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"\nSwapping by value:"<<endl;
+    s1.swapByValue(a,b);
+    cout<<"\nSwapping by pointer:"<<endl;
+    s1.swapByPointer(&a,&b);
+    cout<<"\nSwapping by reference:"<<endl;
+    s1.swapByReference(a,b);
     return 0;
 }
