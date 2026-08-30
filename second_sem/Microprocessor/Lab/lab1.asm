bc642d453b3f1689afd7d39a3fa1e6b262ac96d9
Pranish <pranishrestha@outlook.com>
2026-08-31 04:35:36 +0545

    fix:mp lab16

--- second_sem/Microprocessor/Lab/lab1.asm
+++ second_sem/Microprocessor/Lab/lab1.asm
@@ -1,15 +1,20 @@
-;display a string in 8086
+; Display a string in 8086
 .model small
-.stack 100h  
-.data        
-    text db "hello world!$" 
+.stack 100h
+
+.data
+    text db "hello world!$"
+
 .code
-    begin:
-    mov ax,@data
-    mov ds,ax
-    mov dx,offset text
-    mov ah,09h
+begin:
+    mov ax, @data
+    mov ds, ax
+
+    mov dx, offset text
+    mov ah, 09h
     int 21h
-    mov ah,4ch
-    int 21h                                            a
-    end begin
+
+    mov ah, 4Ch
+    int 21h
+
+end begin
